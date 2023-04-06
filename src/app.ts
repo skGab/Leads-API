a7629d55195ff97efe74ea36f37086295dda4c73
zdeep10 <gabriel.wti02@gmail.com>
2023-04-06 19:52:32 -0300

    Fixing errors

--- src/app.ts
+++ src/app.ts
@@ -16,23 +16,29 @@ app.register(fastifyCors, {
 
 leeds(app);
 
-// users(app, prisma);
+// Unix domain socket
+const unixDomainSocket =
+  '/srv/leeds-api.c09b59be.configr.cloud/etc/nodejs/nodejs.sock';
 
-const rawServer = app.server;
+// TCP/IP address and port
+const host = '0.0.0.0';
+const port = process.env.Port ? Number(process.env.Port) : 3333;
 
-const server: Server = createServer(
-  (req: IncomingMessage, res: ServerResponse) => {
-    rawServer.emit('request', req, res);
-  }
-);
-
-server.listen('/srv/leeds-api.c09b59be.configr.cloud/etc/nodejs/nodejs.sock');
-
-server.on('error', (err) => {
-  console.error('Error starting server:', err);
-  process.exit(1);
-});
-
-server.on('listening', () => {
-  console.log('Server is listening on the Unix domain socket');
-});
+// Check if the environment requires listening on a Unix domain socket
+if (process.env.USE_UNIX_SOCKET) {
+  app.listen({ socket: unixDomainSocket } as any, (err, address) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log(`Server is listening on the Unix domain socket: ${address}`);
+  });
+} else {
+  app.listen({ port, host } as any, (err, address) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log(`Server is listening on ${address}`);
+  });
+}
