09a62e1f2b2e7013e3a3fd9bbab0177c0d4122ce
zdeep10 <gabriel.wti02@gmail.com>
2023-04-06 22:07:14 -0300

    fixing issues

--- src/app.ts
+++ src/app.ts
@@ -8,31 +8,33 @@ app.register(fastifyCors, {
   origin: true,
 });
 
-leeds(app);
+(async () => {
+  await leeds(app);
 
-// Unix domain socket
-const unixDomainSocket =
-  '/srv/leeds-api.c09b59be.configr.cloud/etc/nodejs/nodejs.sock';
+  // Unix domain socket
+  const unixDomainSocket =
+    '/srv/leeds-api.c09b59be.configr.cloud/etc/nodejs/nodejs.sock';
 
-// TCP/IP address and port
-const host = '0.0.0.0';
-const port = process.env.Port ? Number(process.env.Port) : 3333;
+  // TCP/IP address and port
+  const host = '0.0.0.0';
+  const port = process.env.Port ? Number(process.env.Port) : 3333;
 
-// Check if the environment requires listening on a Unix domain socket
-if (process.env.USE_UNIX_SOCKET) {
-  app.listen({ socket: unixDomainSocket } as any, (err, address) => {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-    console.log(`Server is listening on the Unix domain socket: ${address}`);
-  });
-} else {
-  app.listen({ port, host } as any, (err, address) => {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-    console.log(`Server is listening on ${address}`);
-  });
-}
+  // Check if the environment requires listening on a Unix domain socket
+  if (process.env.USE_UNIX_SOCKET) {
+    app.listen({ socket: unixDomainSocket } as any, (err, address) => {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+      console.log(`Server is listening on the Unix domain socket: ${address}`);
+    });
+  } else {
+    app.listen({ port, host } as any, (err, address) => {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+      console.log(`Server is listening on ${address}`);
+    });
+  }
+})();
