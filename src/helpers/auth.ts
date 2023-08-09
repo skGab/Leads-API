90a1a1c70e84fbd50889f846af0179652fe8ecc2
Gabriel Assunção <gabriel.wti02@gmail.com>
2023-08-09 11:05:05 -0300

    Update auth.ts

--- src/helpers/auth.ts
+++ src/helpers/auth.ts
@@ -13,9 +13,9 @@ const credentialsPath =
 // Set up the auth object using environment variables or default values
 const auth = {
   keyFilename: path.join(__dirname, credentialsPath),
-  projectId: 'black-beans-dados',
+  projectId: 'projectID',
 };
 
 // Set up BigQuery client and dataset
 export const bigqueryClient = new BigQuery(auth);
-export const db_dataset = bigqueryClient.dataset('teste');
+export const db_dataset = bigqueryClient.dataset('dataset');
